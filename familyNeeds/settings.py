9ec2b62015fe645815ed49a8623a15b722db440f
Minerva <rezaiealireza884@gmail>
2021-07-02 22:08:00 +0430

    added alowed host

--- familyNeeds/settings.py
+++ familyNeeds/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q&(&g4-@n5ksfvj10y-lg1nau1(4rasur9y#hbsn2a_79zqf8%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://familyneeds.herokuapp.com/']
 
 
 # Application definition
