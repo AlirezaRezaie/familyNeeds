e6a49790b055b981e61cddae5874aa190b77ce6a
Minerva <rezaiealireza884@gmail>
2021-07-02 21:58:33 +0430

    updated STATIC_ROOT

--- familyNeeds/settings.py
+++ familyNeeds/settings.py
@@ -120,10 +120,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
