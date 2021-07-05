659a9fab749f85bee24055b631bf153dae82c9d8
Minerva <rezaiealireza884@gmail>
2021-07-05 14:45:15 +0430

    debug false

--- familyNeeds/settings.py
+++ familyNeeds/settings.py
@@ -120,5 +120,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if not DEBUG:
-    django_heroku.settings(locals())
+django_heroku.settings(locals())
