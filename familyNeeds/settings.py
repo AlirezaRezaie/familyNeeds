46855becbd1973e05c67a272cf1ba8a44e8edda0
Minerva <rezaiealireza884@gmail>
2021-07-05 14:58:01 +0430

    updated settings.py

--- familyNeeds/settings.py
+++ familyNeeds/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q&(&g4-@n5ksfvj10y-lg1nau1(4rasur9y#hbsn2a_79zqf8%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://familyneeds.herokuapp.com/','familyneeds.herokuapp.com', "localhost", "127.0.0.1"]
 
@@ -113,11 +113,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = "/static/"
+if DEBUG:
+   STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles')
+    ]
+elif not DEBUG:
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+if not DEBUG:
+   django_heroku.settings(locals())
