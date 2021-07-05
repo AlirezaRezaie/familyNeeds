24c7c23fd7c9057d8849e25e6741238a1b6ce16d
Minerva <rezaiealireza884@gmail>
2021-07-05 13:44:26 +0430

    debug false

--- familyNeeds/settings.py
+++ familyNeeds/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q&(&g4-@n5ksfvj10y-lg1nau1(4rasur9y#hbsn2a_79zqf8%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://familyneeds.herokuapp.com/', "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['https://familyneeds.herokuapp.com/','familyneeds.herokuapp.com', "localhost", "127.0.0.1"]
 
 # Application definition
 
