0b04f13b8827653e57bea144d4007a0181a677ec
Minerva <rezaiealireza884@gmail>
2021-07-05 13:36:43 +0430

    debug false

--- familyNeeds/settings.py
+++ familyNeeds/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q&(&g4-@n5ksfvj10y-lg1nau1(4rasur9y#hbsn2a_79zqf8%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://familyneeds.herokuapp.com/', "localhost", "127.0.0.1"]
 
